552bad872ee3c3667787cd851cca40d4b0d69f08
Evan-otero <Evan.otero19@gmail.com>
2019-08-21 22:54:57 +0000

    added string length and semicolons to js

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-let parrot=["A dog", "A cat", "A hamster","My wife","My brother"]
-let table=[" ate"," kicked"," stole"," broke"," destroyed"]
-let door=[" my homework"," my car"," my backpack"," my phone"," my keys"]
-let clock=[" yesterday"," on saturday"," last week"," this morning"," last night"]
-let excuse= parrot[Math.floor(Math.random() * 5)] + table[Math.floor(Math.random() * 5)] + door[Math.floor(Math.random() * 5)] + clock[Math.floor(Math.random() * 5)]
-
-console.log(excuse)
-document.write(excuse)
+//function onLoad=
+{
+let parrot=["A dog", "A cat", "A hamster","My wife","My brother"];
+let table=[" ate"," kicked"," stole"," broke"," destroyed"];
+let door=[" my homework"," my car"," my backpack"," my phone"," my keys"];
+let clock=[" yesterday"," on saturday"," last week"," this morning"," last night"];
+let excuse= parrot[Math.floor(Math.random() * parrot.length)]
+            + table[Math.floor(Math.random() * table.length)]
+            + door[Math.floor(Math.random() * door.length)]
+            + clock[Math.floor(Math.random() * clock.length)];
+return excuse;
+}
+console.log(excuse);
